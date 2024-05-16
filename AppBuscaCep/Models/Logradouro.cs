dfe1d8d30ac72e77f87a75a709779c6f102f09d4
Usuario <Usuario@CPD03-M08>
2024-05-16 15:02:52 -0300

    Configuration class

--- AppBuscaCep/Models/Logradouro.cs
+++ AppBuscaCep/Models/Logradouro.cs
@@ -8,5 +8,16 @@ namespace AppBuscaCep.Models
 {
     internal class Logradouro
     {
+        public int CEP { get; set; }
+        public string id_logradouro { get; set; }
+        public string tipo { get; set; }
+        public string descricao { get; set; }
+        public string id_cidade { get; set; }
+        public string UF { get; set; }
+        public string complemento { get; set; }
+        public string descricao_sem_numero {  get; set; }
+        public string descricao_cidade { get; set; }
+        public string codigo_cidade_ibge { get; set; }
+        public string descricao_bairro { get; set; }
     }
 }
--- AppBuscaCep/Services/DataService.cs
+++ AppBuscaCep/Services/DataService.cs
@@ -1,12 +1,36 @@
-﻿using System;
+﻿using AppBuscaCep.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
+using System.Net.Http;
 
 namespace AppBuscaCep.Services
 {
-    internal class DataService
+    public class DataService
     {
+        public static async Task<Endereco> GetEnderecoByCep(string cep)
+        {
+            Endereco end;
+            using (HttpClient client = new HttpClient())
+            {
+                string url = "https://cep.metoda.com.br/endereco/by-cep?cep" + cep;
+                HttpResponseMessage response= await client.GetAsync(url);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    string json = response.Content.ReadAsStringAsync().Result;
+
+                    end = JsonConvert.DeserializeObject<Endereco>(json);
+                }
+                else
+                {
+                    throw new Exception(response.RequestMessage.Content.ToString());
+                }
+                return end;
+            } 
+        }
     }
 }
